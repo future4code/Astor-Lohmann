130638da1194670c158fbae9b2ac84b43e8c9334
Astor <astorlohmann@gmail.com>
2020-07-30 16:58:27 -0300

    entrega

---
+++ semana3/Aula12 - loops/index.js
@@ -0,0 +1,32 @@
+// EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO:
+
+// 1. O codigo está adicionando o numero do indice a soma total, o indice aumenta em uma unidade a cada vez
+// que soma o indice a soma total, o resultado final é 10.
+
+// 2. Será impresso um lista de todos os elementos menores de 18 que estão no arrray.
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+// 3a.
+// const lista = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+// for (let numero of lista) {
+  
+// 		console.log(numero)
+// 	}
+
+// 3b.
+// const lista = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+// for (let numero of lista) {
+  
+// 		console.log(numero/10)
+// 	}
+
+// 3c.
+const lista = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+let numero 
+for (let numero of lista; ) {
+    if (numero = numero%2) {
+  
+        console.log(numero)
+	}
+}
