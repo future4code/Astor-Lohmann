c14497636249dbfff949c59fe7fd265af048ce6b
Astor <astorlohmann@gmail.com>
2020-08-04 16:33:27 -0300

    entrega

---
+++ semana4/Aula14-planner/index.js
@@ -0,0 +1,32 @@
+function addTarefa(){
+    const pegarTarefa = document.getElementById("tarefa").value
+    let tarefa =
+   
+    const semana = document.getElementById("dias-semana").value
+   
+
+
+    
+console.log(tarefa);
+console.log(semana)
+
+
+
+
+}
+//    switch(diaSemana) {
+//        case "domingo":
+//         tarefa.innerHTML += `<p>${tarefa}</p>`
+//    }
+//    switch (catejogo) {
+//                     case "1":
+//                         if (tipoJogo === "DO") {
+//                             console.log("Valor do ingresso: " + precoSF1)
+//                             console.log("Valor total: " + precoSF1*qntIngressos)
+//                         } else if (tipoJogo === "IN") {
+//                             console.log("Valor do ingresso:US$ " + precoSF1/dolar)
+//                             console.log("Valor total:US$ " + (precoSF1*qntIngressos)/dolar)
+//                         }
+//                         break
+    
+// }
