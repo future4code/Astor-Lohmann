355f18d9580a8d207cf62d7a4c74fe3b79079d38
Astor <astorlohmann@gmail.com>
2020-08-07 17:07:40 -0300

    entrega exercicios

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -15,3 +15,75 @@
 //4. Há uma array com elementos definidos, a função definie a variavel numero1 se o elemento do array for maior de 0, e define a variavél numero2 se for maior
 
 
+// EXERCICIO DE LÓGICA DE PROGRAMAÇÃO---------------------------------------------
+
+// 1. Há 3 maneiras de iteração: while, for e for of:
+
+
+// while (true) {
+//     console.log("Olá, mundo");
+//   }
+
+// for (i = 0; i < a.length; i++) {
+//   if (a[i] == theValue) {
+//     break;
+//   }
+// }
+
+// let arr = [3, 5, 7];
+// for (let i of arr) {
+//     console.log(i);
+//  }
+
+// 2a. false
+// 2b. true
+// 2c.true
+// 2d. false
+// 2e false
+
+// 3.O código não funciona, está incompleto, não contição difinida para o N, bem como para o laço dos 
+// números pares (como vai percorrer e mostrar os numeros no console)
+
+// const quantidadeDeNumerosPares
+// let i = 0
+// while(i <= quantidadeDeNumerosPares) {
+//   console.log(i*2)
+// }
+
+// 4. 
+// const ladosTriagulo = [a , b, c]
+
+
+// function tipoTringulo (ladosTriagulo) {
+    
+//     switch(ladosTriagulo){
+//         case (a === b && a === c && b === c):
+//             alert("o trigulo é Equilátero");
+//     break;
+//     case (a === b && a !=== c && b !=== c):
+//         alert("o trigulo é Isósceles");
+//         break;
+//     case (a !=== b && a !=== c && b !=== c):
+//         alert("o trigulo é Escaleno");
+//         break;
+        
+//     }
+// }
+
+// 5.
+
+let number1 = 15
+let number2 = 30
+
+function verificaNumeros (number1 , number2){
+    function numeroMaior( number1, number2 ){
+        if ( number1 > number2 ) {
+       console.log("O " number1 " é maior")
+    }if else (number1 < number2) {
+        console.log("O " number2 "é maior")
+    }
+}
+
+}
+
+verificaNumeros
