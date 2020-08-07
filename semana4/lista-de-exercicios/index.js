115291d269b886268d040586d942b5e102b2e0c3
Astor <astorlohmann@gmail.com>
2020-08-07 15:32:49 -0300

    exercicios 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,17 @@
+// LISTA DE EXERCICIOS
+
+
+// EXERCICIOS INTERPRETAÇÃO CÓDIGO-------------------------------------------------
+
+// 1. O código abre uma janela prompt que pergunta ao usuário qual a cotação em dolar, convertendo-a em um valor numérico, em seguida multiplica
+// multiplica a mesma cotação pelo valor já definido de 100.
+
+// 2. O código está declarando uma função com dois parametros, há um switch case para o parametro
+// tipoDeInvetimento que define o valorAposInvetimeto.
+
+// 3. Há uma array com números definidos, e duas arrays vazias, uma função "for of" empurra os elementos pares
+// para a array1 e os outros(impares) para  array2
+
+//4. Há uma array com elementos definidos, a função definie a variavel numero1 se o elemento do array for maior de 0, e define a variavél numero2 se for maior
+
+
