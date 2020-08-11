209c15bdb7e1a6a53685c4490ff6752e5074f94c
Astor <astorlohmann@gmail.com>
2020-08-11 18:21:11 -0300

    entrega

---
+++ semana5/labedin/src/img/MinhaImagem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './me.jpg'
+
+function MinhaImagem() {
+    return (
+        <div className="imagem-minha">
+            <img src={ './me.jpg' } />
+            
+        </div>
+    )
+}
+
+export default MinhaImagem;
