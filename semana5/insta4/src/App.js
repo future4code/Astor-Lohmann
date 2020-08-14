c42efa3ecf3ccfe06da3b189c278cb57095c1445
Astor <astorlohmann@gmail.com>
2020-08-13 23:21:39 -0300

    entrega

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,24 +3,41 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+
+  state = {
+    // Lista de pessoas colocada no estado
+    posts: [
+      // O objeto abaixo representa uma pessoa. Ele possui
+      // duas propridades: nome e email.
+      {
+        nome: "Paulinha",
+        fotousuario: {https://picsum.photos/50/50'paula@f4.com"},
+        postfoto: "9959-5254"
+      },
+      {
+        nome: "Jair",
+        fotousuario: "https://picsum.photos/50/51",
+        postfoto: {https://picsum.photos/200/150"
+      },
+      {
+        nome: "Inácio",
+        fotousuario: "https://picsum.photos/50/52",
+        postfoto: "https://picsum.photos/200/152"
+      }
+    ]
+  };
   render() {
+    const novoPost = this.state.posts.map((post) => {
+      return (
+        <li>
+          {post.nome} - {post.fotousuario} - {post.postfoto}
+        </li>
+      );
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'Paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Jair'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
-        <Post
-          nomeUsuario={'Inácio'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
+        {novoPost}
       </div>
     );
   }
