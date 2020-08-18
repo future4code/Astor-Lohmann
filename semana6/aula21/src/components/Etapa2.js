797dc9021c5590430dc74183b6cff54353e0f347
Astor Lohmann Palmeira <astorlohmann@gmail.com>
2020-08-17 23:01:42 -0300

    entrega

---
+++ semana6/aula21/src/components/Etapa2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Etapa2 extends React.Component {
+    render() {
+      return (
+        <div>
+          <h2>Etapa2 - Informações do Ensino Superior</h2>
+          <h3>Qual o curso?</h3>
+          <input></input>
+          <h3>Qual a unidade de ensino?</h3>
+          <input></input>
+          
+        </div>
+      );
+    }
+  }
+  
+  export default Etapa2;
