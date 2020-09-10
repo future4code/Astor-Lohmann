f78c0935f159d4a13bc80954b214e12373b91afa
Astor Lohmann Palmeira <astorlohmann@gmail.com>
2020-09-10 19:42:10 -0300

    projetobase

---
+++ semana9/projeto/astromatch/src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+    <div>
+      <div>
+          Tinder Card
+      </div>
+      <div>
+          buttons
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ semana9/projeto/astromatch/src/Components/Match.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Match() {
+  return (
+    <div className="App">
+      Match Screen
+
+    </div>
+  );
+}
+
+export default Match;
